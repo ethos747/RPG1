d11056a9979ea9664c5f24e6a91c544ac045f7b8
ethos747 <89998096+ethos747@users.noreply.github.com>
2021-09-15 11:54:20 -0600

    Update RPG1.py

--- RPG1.py
+++ RPG1.py
@@ -7,10 +7,14 @@
 
 #Main functionalities
   #Menu system
-    #Options for out of combat []Interact, []Explore, []Inventory
+    #Options for out of combat []Interact, []Explore, []Inventory, []Status
       #Interact
+        #after room features have been listed, interactable options are given when this is chosen
       #Explore
+        #Select this to change what room you are in out of combat
       #Inventory
+        #Lists all items in inventory, [#] infront
+          #Can view each items description by selectiong said [#]
     #Options for in-combat []Attack, []Dodge, []Inventory
       #Attack
         #give options of various weaponry
@@ -19,10 +23,11 @@
         #List chance to dodge [%]
         #Option for the player to do so
       #Inventory
-        #Same as lsited, except certain objects cannot be used
+        #Same as lsited, except certain objects cannot be used, message displayed
       
   #Game mechanics
     #Combat
+      #
     #Weapons
   #
 #Gameplay
