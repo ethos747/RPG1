6d5c2987256c929a95d6f2f1aaf4693e8b0d7cfc
ethos747 <89998096+ethos747@users.noreply.github.com>
2021-09-15 11:30:24 -0600

    Update RPG1.py

--- RPG1.py
+++ RPG1.py
@@ -1,1 +1,12 @@
-print("Hello")
+# Course: CS 30
+# Period: 3
+# Date created: 9/15/2021
+# Date last modified: 9/15/2021
+# Name: Ethan Behl
+# Description: Text-based RPG
+
+
+
+
+
+print ("Hello")
